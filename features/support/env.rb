0afb066ae9df5ad9349b87d635623932ec74f629
Gaelan D'costa <gdcosta@gmail.com>
2015-04-20 09:12:16 -0400

    Remove debug statements from cucumber.

--- features/support/env.rb
+++ features/support/env.rb
@@ -2,4 +2,3 @@ require 'aruba/cucumber'
 
 PROJECT_ROOT = File.dirname(File.expand_path('../../', __FILE__))
 ENV['PATH'] = File.join(PROJECT_ROOT, '/exe') + File::PATH_SEPARATOR + ENV['PATH']
-puts ENV['PATH']
